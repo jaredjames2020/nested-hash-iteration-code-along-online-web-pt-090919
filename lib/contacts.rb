79e6686e45ef2458f981717f3087ffb2e7a51fbf
Jared S <jaredjames2020@gmail.com>
2019-09-21 23:51:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,11 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"].each do |k,v|
-      if k == :favorite_ice_cream_flavors
-      contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete("strawberry")
-      #binding.pry
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
       end
     end
-    contacts["Freddy Mercury"][:favorite_ice_cream_flavors]
+  end
 end
